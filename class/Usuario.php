ee73e87e5a6a76e113e5cb3a5e052b6ebc0c56cb
AlissonTS <alissonts2010@hotmail.com>
2018-09-16 10:48:49 -0300

    Criação da classe Usuario com método de select por ID

---
+++ class/Usuario.php
@@ -0,0 +1,68 @@
+<?php
+
+class Usuario {
+		private $idusuario;
+		private $deslogin;
+		private $dessenha;
+		private $dtcadastro;
+
+		public function getIdUsuario() {
+			return $this->idusuario;
+		}
+
+		public function setIdUsuario($idusuario) {
+			$this->idusuario = $idusuario;
+		}
+
+		public function getDeslogin() {
+			return $this->deslogin;
+		}
+
+		public function setDeslogin($deslogin) {
+			$this->deslogin = $deslogin;
+		}
+
+		public function getDessenha() {
+			return $this->dessenha;
+		}
+
+		public function setDessenha($dessenha) {
+			$this->dessenha = $dessenha;
+		}
+
+		public function getDtcadastro() {
+			return $this->dtcadastro;
+		}
+
+		public function setDtcadastro($dtcadastro) {
+			$this->dtcadastro = $dtcadastro;
+		}
+
+		public function loadById($id) {
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+				":ID"=>$id
+			));
+
+			if (count($results) > 0) {
+				$row = $results[0];
+
+				$this->setIdUsuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}
+		}
+
+		public function __toString() {
+			return json_encode(array(
+				"idusuario"=>$this->getIdUsuario(),
+				"deslogin"=>$this->getDeslogin(),
+				"dessenha"=>$this->getDessenha(),
+				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+			));
+		}
+}
+
+?>
