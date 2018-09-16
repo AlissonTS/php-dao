120f10fe3cdc693a86db81c6c29c93a812f47458
AlissonTS <alissonts2010@hotmail.com>
2018-09-16 14:43:23 -0300

    Inserção de métodos de listagem na classe Usuario

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 	require_once("config.php");
 
+	/* Retorna um usuário
 	$usuario = new Usuario();
 	$usuario->loadById(1);
+	echo $usuario; */
+
+	/* Carrega a lista de usuários
+	$lista = Usuario::getList();
+	echo json_encode($lista); */
+
+	/* Carrega uma lista de usuários buscando pelo login
+	$search = Usuario::search("root");
+	echo json_encode($search); */
+
+	// Carrega um usuário usando um login e a senha
+	$usuario = new Usuario();
+	$usuario->login("root", "123");
 
 	echo $usuario;
 ?>
